1757935adeca01d236cd62022c649fdf2529e122
Karl Matthes <karlmatthes@gmail.com>
2021-01-23 01:51:10 +0000

    Automatically backed up by Learn

---
+++ sandbox.rb
@@ -0,0 +1,7 @@
+def greeting_a_person(holiday = "Festivus", name = "Bob")
+  puts "Hello, #{name}! Happy #{holiday}!"
+end
+
+greeting_a_person
+greeting_a_person("Easter")
+greeting_a_person("Third Wednesday","Jane")
